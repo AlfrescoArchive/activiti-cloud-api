aafa064b6fac2043bf37940123870c588664dc26
Marcello Teodori <marcello.teodori@alfresco.com>
2019-11-21 17:40:28 +0000

    AAE-628 added events info to README and JSON schema generated and copy pasted by now

---
+++ activiti-cloud-api-events/src/test/java/org/activiti/cloud/api/events/CloudRuntimeEventTypeTest.java
@@ -0,0 +1,40 @@
+package org.activiti.cloud.api.events;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.activiti.api.process.model.events.BPMNActivityEvent;
+import org.activiti.api.process.model.events.BPMNErrorReceivedEvent;
+import org.activiti.api.process.model.events.BPMNMessageEvent;
+import org.activiti.api.process.model.events.BPMNTimerEvent;
+import org.activiti.api.process.model.events.MessageDefinitionEvent;
+import org.activiti.api.process.model.events.ProcessDefinitionEvent;
+import org.activiti.api.process.model.events.ProcessRuntimeEvent;
+import org.activiti.api.process.model.events.SequenceFlowEvent;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CloudRuntimeEventTypeTest {
+
+    private <E extends Enum<E>> List<String> getEnumValuesAsStringList(Class<E> enumClass) {
+        return EnumSet.allOf(enumClass).stream()
+                .map(Enum::name).collect(Collectors.toList());
+    }
+
+    @Test
+    public void shouldContainAllValueFromEventEnums() {
+        List<String> enumValues = this.getEnumValuesAsStringList(CloudRuntimeEventType.class);
+
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(BPMNActivityEvent.ActivityEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(BPMNErrorReceivedEvent.ErrorEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(BPMNTimerEvent.TimerEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(BPMNMessageEvent.MessageEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(ProcessDefinitionEvent.ProcessDefinitionEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(SequenceFlowEvent.SequenceFlowEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(ProcessRuntimeEvent.ProcessEvents.class));
+        assertThat(enumValues).containsAll(this.getEnumValuesAsStringList(MessageDefinitionEvent.MessageDefinitionEvents.class));
+    }
+
+}
